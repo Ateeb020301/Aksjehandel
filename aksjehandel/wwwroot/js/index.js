37562dd4b192399a1a1cd1c63fbe0e7dd19dca67
moha0102 <mohammedak0102@gmail.com>
2022-10-21 08:04:56 +0200

    Design for Dashboard, added CSS

--- aksjehandel/wwwroot/js/index.js
+++ aksjehandel/wwwroot/js/index.js
@@ -1,7 +1,29 @@
 ﻿fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d')
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => tester(data));
 
+
+function tester(aksjer) {
+    console.log(aksjer); 
+    let img = document.getElementsByClassName('coinImg');
+    let coin = document.getElementsByClassName('coin');
+    let symbol = document.getElementsByClassName('symbol');
+    let price = document.getElementsByClassName('price');
+    let supply = document.getElementsByClassName('circulatingSupply');
+    
+    for(i = 0; i < 10; i++) {
+        img[i].src = aksjer[i].image;
+        coin[i].innerHTML = aksjer[i].name;
+        let smbl = aksjer[i].symbol;
+        symbol[i].innerHTML = smbl.toUpperCase();
+        price[i].innerHTML = `$${aksjer[i].current_price}`;
+        let cSupply = aksjer[i].circulating_supply;
+        supply[i].innerHTML = Math.round(cSupply).toLocaleString().replace(/,/g, '.');
+        
+    }
+    
+}
+  /*
 window.onload = function () {
     hentAlleKunder();
 }
@@ -42,3 +64,4 @@ function slettKunde(id) {
 
     });
 };
+*/
